99b9a3cfdff201f6f618cffbc5e26564e5672c13
Lexutka <lexutka@gmail.com>
2022-06-18 15:58:04 +0700

    secret_key add

--- MKclient/settings.py
+++ MKclient/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-from .secret_key import key
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,7 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = key
+if os.path.exists('secret_key.py'):
+   from .secret_key import key
+   SECRET_KEY = key
+else:
+    SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
